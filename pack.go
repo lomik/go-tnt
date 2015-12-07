d119fc73cada851dfbebc53da673823cdf331a35
Roman Lomonosov <r.lomonosov@gmail.com>
2015-12-07 16:32:46 +0300

    PackB

--- pack.go
+++ pack.go
@@ -1,1 +1,5 @@
 package tnt
+
+func PackB(value int) []byte {
+	return []byte{uint8(value % 0x100)}
+}
--- pack_test.go
+++ pack_test.go
@@ -4,11 +4,17 @@ import (
 	"encoding/base64"
 	"fmt"
 	"log"
+	"math/rand"
 	"os/exec"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
 
 func python(code string) []byte {
 	cmd := exec.Command("python", "-c", code)
@@ -22,11 +28,12 @@ func python(code string) []byte {
 	return []byte(out)
 }
 
-func pythonIproto(code string) []byte {
+func pythonIproto(code string, args ...interface{}) []byte {
+	codeWithParams := fmt.Sprintf(code, args...)
 	res := python(
 		fmt.Sprintf(
 			"import python_iproto, sys; sys.stdout.write(str(python_iproto.%s).encode(\"base64\").strip())",
-			code,
+			codeWithParams,
 		),
 	)
 
@@ -37,6 +44,13 @@ func pythonIproto(code string) []byte {
 	}
 
 	return data
+}
+
+func randomInt(min int, max int) int {
+	if min == max {
+		return min
+	}
+	return rand.Intn(max-min) + min
 }
 
 func TestPython(t *testing.T) {
@@ -53,4 +67,22 @@ func TestPythonIproto(t *testing.T) {
 		[]byte{0x04, 0x01, 0x00, 0x00, 0x00},
 		pythonIproto("pack_int(1)"),
 	)
+}
+
+func TestPackB(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal(
+		pythonIproto("struct_B.pack(0)"),
+		PackB(0),
+	)
+
+	for x := uint(0); x < 8; x++ {
+		value := randomInt(1<<x, (1<<(x+1))-1)
+
+		assert.Equal(
+			pythonIproto("struct_B.pack(%d)", value),
+			PackB(value),
+		)
+	}
 }
