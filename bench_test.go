561f022137b7d595e82bec0bbccc7e05f7dc3c22
Roman Lomonosov <r.lomonosov@gmail.com>
2015-12-07 23:20:29 +0300

    benchmarks

--- bench_test.go
+++ bench_test.go
@@ -3,16 +3,15 @@ package tnt
 import "testing"
 
 func BenchmarkSelectPack(b *testing.B) {
-	query := &Select{
-		Values: Tuple{PackL(11), PackL(12)},
-		Space:  10,
-		Offset: 13,
-		Limit:  14,
-		Index:  15,
-	}
-
 	// run the Fib function b.N times
 	for n := 0; n < b.N; n++ {
+		query := &Select{
+			Values: Tuple{PackL(11), PackL(12)},
+			Space:  10,
+			Offset: 13,
+			Limit:  14,
+			Index:  15,
+		}
 		query.Pack()
 	}
 }
